45cdd99cc4657cc73ad26b3253fef92040816cd9
AnaStanescu22 <stanescuanamaria22@yahoo.com>
2022-07-28 16:15:58 +0300

    Add Compose

---
+++ app/src/main/java/com/lehtimaeki/askold/landingscreen/LandingCompose.kt
@@ -0,0 +1,31 @@
+package com.lehtimaeki.askold.landingscreen
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.AbstractComposeView
+
+class LandingCompose @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : AbstractComposeView(context, attrs, defStyle) {
+
+    var text by mutableStateOf("")
+
+    @Composable
+    override fun Content() {
+        SetLabelText(text)
+    }
+}
+
+@Composable
+fun SetLabelText(
+    text: String
+) {
+    Text(text)
+}
