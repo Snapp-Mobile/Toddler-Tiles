ea1a248d6a9ceda340ee90167b085ae503fbfd53
AnaStanescu22 <stanescuanamaria22@yahoo.com>
2022-10-10 10:54:24 +0300

    Add Hilt

--- app/src/main/java/com/lehtimaeki/askold/data/UserPreferences.kt
+++ app/src/main/java/com/lehtimaeki/askold/data/UserPreferences.kt
@@ -1,17 +1,13 @@
 package com.lehtimaeki.askold.data
 
-import android.content.Context
 import android.content.SharedPreferences
-import com.lehtimaeki.askold.MyApplication
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class UserPreferences @Inject constructor() {
-    private val sharedPref: SharedPreferences? =
-        MyApplication.getAppContext()
-            ?.getSharedPreferences(ASKOLD_SHARED_PREFS, Context.MODE_PRIVATE)
+@Singleton
+class UserPreferences @Inject constructor(private val sharedPref: SharedPreferences) {
 
     fun saveUserName(userName: String) {
-        sharedPref ?: return
         with(sharedPref.edit()) {
             putString(KEY_USER_NAME, userName)
             apply()
@@ -19,11 +15,11 @@ class UserPreferences @Inject constructor() {
     }
 
     fun getUserName(): String? {
-        return sharedPref?.getString(KEY_USER_NAME, null)
+        return sharedPref.getString(KEY_USER_NAME, null)
     }
 
     companion object {
         private const val KEY_USER_NAME = "name"
-        private const val ASKOLD_SHARED_PREFS = "askold_shared_prefs"
+        const val ASKOLD_SHARED_PREFS = "askold_shared_prefs"
     }
 }
